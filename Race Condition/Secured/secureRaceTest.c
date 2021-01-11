3ba0af0881306128ee99d914a928eb4363cb3717
jonnycoombs <73531729+jonnycoombs@users.noreply.github.com>
2021-01-11 12:27:11 +0000

    Final testing and Commit for Secured race Conditon code

--- Race Condition/Secured/secureRaceTest.c
+++ Race Condition/Secured/secureRaceTest.c
@@ -8,41 +8,36 @@
 // Race Condition (TOCTUO)
 
 int main(int argc, char* argv[]) {
-	//Declaring variables
-    int fd;
+// Declaring Variables
+    int fileData;
     int size = 0;
-    char buf[512];
-    //This functions checks the input is correctly formatted and if not explains how to correctly use the program
-    if(argc != 2) {
-        printf("usage: %s <file>\n", argv[0]);
-        exit(1);
-    }
+    char buf[256];
+
+    
 //The below line was moved from the bottom so that the file is opened before it is checked if it is owned by root
 // This prevents the ability for a time-of-check to time-of-use race condtion to occur
-    fd = open(argv[1], O_RDONLY);
+    fileData = open(argv[1], O_RDONLY);
 
-    // This function checks if the above fd variable contains a 1 (success) showing the file can open if not then it prints an error
-    if(fd <= 0)
+    //This function checks if the above fileData variable contains the open file if not then it prints an error
+    if(fileData <= 0)
     {
-        fprintf(stderr, "Could not open inputted file %s\n", argv[1]);
-        exit(1);
-    }
-//Getting information about the file after it has already been read into memory
-    struct stat stat_data;
-    if (fstat(fd, &stat_data) < 0) {
-        fprintf(stderr, "Failed to get file information %s: %s\n", argv[1], strerror(errno));
-        exit(1);
-    }
-    // If the file is owned by root it will print that and exit the program.
-    if(stat_data.st_uid == 0)
-    {
-        fprintf(stderr, "The File %s is owned by root user\n", argv[1]);
+        fprintf(stdout, "Couldn't open %s\n", argv[1]);
         exit(1);
     }
 
-    //This function writes the read file into the bufer and then prints it until size = 0
+	struct stat file_info;
+
+//This function checks if the file is owned by root
+//st_uid hgets the user ID of the owner and 0 means root
+    if(file_info.st_uid == 0)
+    {
+        fprintf(stdout, "Can't open, this file %s is owned by root\n", argv[1]);
+        exit(1);
+    }
+
+//This function writes the read file into the buffer and then prints it until size = 0
     do {
-        size = read(fd, buf, 512);
+        size = read(fileData, buf, 256);
         write(1, buf, size);
     } while(size>0);
 
